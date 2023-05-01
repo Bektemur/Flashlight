5f39ff365a8779835314fc9668b0944324e16da4
Bektemur <bektemur@bk.ru>
2023-05-01 20:00:04 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Device.Gpio;
+
+int ledPin = 24; //GPIO24 is pin 18 on RPi
+int ledOnTime = 1000; //led on time in ms
+int ledOffTime = 500; //led off time in ms
+
+using GpioController controller = new();
+controller.OpenPin(ledPin, PinMode.Output);
+
+Console.CancelKeyPress += (s, e) =>
+{
+    controller.Dispose();
+};
+
+while (true)
+{
+    controller.Write(ledPin, PinValue.High);
+    Thread.Sleep(ledOnTime);
+
+    controller.Write(ledPin, PinValue.Low);
+    Thread.Sleep(ledOffTime);
+}
